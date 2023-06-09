61f9ace1d6e54f4b6ae89b3e6c6c014409b8943e
ewq897456321 <ltesr125124015@gmail.com>
2023-06-09 09:11:06 +0800

    add user appointment show service driver

--- Users/table.py
+++ Users/table.py
@@ -5,6 +5,7 @@ from .models import *
 class Appointment(tables.Table):
     appointment_user = Column(accessor='appointment_user.username', verbose_name='預約帳號')
     manager = Column(accessor='manager.manager_id', verbose_name='客服ID')
+    service_driver = Column(accessor='schedules.driver.name', verbose_name='服務司機')
 
     class Meta:
         model = Appointment
